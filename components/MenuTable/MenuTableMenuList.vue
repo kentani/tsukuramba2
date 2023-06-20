b95f214c9ad7306d8aabe21042cd20722ba2b468
Kento Tani <k.tani0212@gmail.com>
2023-06-20 12:33:30 +0900

    選択済みメニューのUIを変更

--- components/MenuTable/MenuTableMenuList.vue
+++ components/MenuTable/MenuTableMenuList.vue
@@ -20,7 +20,17 @@
           :src="menu.url || menu.ogp?.image || defaultImage"
           cover
         >
+
+          <div
+            v-if="selectedMenuIds.includes(menu.id)"
+            class="pa-2 font-weight-bold text-body-2"
+            style="background: rgb(var(--v-theme-brown1)); opacity: 0.8; height: 120px;"
+          >
+            選択済み
+          </div>
+
           <v-card-title
+            v-else
             class="py-0 px-1 font-weight-bold text-body-2"
             style="background: rgb(var(--v-theme-brown1)); opacity: 0.8;"
           >
@@ -41,7 +51,7 @@ import { MenuListStoreType } from "@/composables/menus/use-menu-list"
 import MenuListStoreKey from "@/composables/menus/use-menu-list-key"
 
 const { menusHash } = inject(MenuTableListStoreKey) as MenuTableListStoreType
-const { currentMenuTable, setCurrentMenuTable, setSelectedMenus } = inject(MenuTableStoreKey) as MenuTableStoreType
+const { currentMenuTable, selectedMenus, setCurrentMenuTable, setSelectedMenus } = inject(MenuTableStoreKey) as MenuTableStoreType
 const { filteredMenus } = inject(MenuListStoreKey) as MenuListStoreType
 
 const defaultImage = ref('/images/pasta.png')
@@ -50,9 +60,19 @@ const menus = computed(() => {
   return filteredMenus.value
 })
 
+const selectedMenuIds = computed(() => {
+  return selectedMenus.value.map((m: any) => m.id)
+})
+
 const onClickMenu = (menu: any) => {
   const menus = currentMenuTable.value.menus
-  menus.push(menu.id)
+
+  if(selectedMenuIds.value.includes(menu.id)) {
+    const index = menus.indexOf(menu.id)
+    menus.splice(index, 1)
+  } else {
+    menus.push(menu.id)
+  }
 
   const menuTable = {
     ...currentMenuTable.value,
